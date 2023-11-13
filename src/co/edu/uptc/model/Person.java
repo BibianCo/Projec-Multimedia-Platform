1163806633132b46f97f50af373ff910a1d7a602
juan-Riapira <juan.fernandez07@uptc.edu.co>
2023-11-13 17:40:59 -0500

    <PM-27> Create packages And Class

---
+++ src/co/edu/uptc/model/Person.java
@@ -0,0 +1,38 @@
+package co.edu.uptc.model;
+
+public abstract class Person {
+    private String firstName;
+    private String lastName;
+    private short age;
+
+    public Person(String firstName, String lastName, short age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public short getAge() {
+        return age;
+    }
+
+    public void setAge(short age) {
+        this.age = age;
+    }
+
+}
