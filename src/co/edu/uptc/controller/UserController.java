fc2f82952ba899b127fa7128a9bb4face223a94e
Bibian Corredor <bibian.corredor@gmail.com>
2023-12-05 18:55:47 -0500

    create user create method and add it to user list

--- src/co/edu/uptc/controller/UserController.java
+++ src/co/edu/uptc/controller/UserController.java
@@ -2,13 +2,16 @@ package co.edu.uptc.controller;
 
 import java.util.ArrayList;
 
+import co.edu.uptc.model.Administrator;
 import co.edu.uptc.model.Multimedia;
+import co.edu.uptc.model.Plan;
 import co.edu.uptc.model.User;
 
 public class UserController {
     private User user;
-
+    private Administrator administrator;
     private ArrayList<Multimedia> playMultimedias = new ArrayList<>();
+    private ArrayList<User> users = new ArrayList<>();
 
     public boolean addListHistory(Multimedia multimedia) {
         if (multimedia.isReproduce()) {
@@ -21,6 +24,16 @@ public class UserController {
 
     public ArrayList<Multimedia> showListHistory() {
         return user.getPlaylist();
+    }
+
+    public boolean addUser(String name, String email, String password, String userName, Plan plan) {
+        user = new User(name, email, password, userName, plan);
+        if (user != null) {
+            users.add(user);
+            administrator.setUsers(users);
+            return true;
+        }
+        return false;
     }
 
 }
--- src/co/edu/uptc/model/Administrator.java
+++ src/co/edu/uptc/model/Administrator.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class Administrator extends Person {
     private int code;
+    private ArrayList<User> users = new ArrayList<>();
 
     public Administrator(String name, String email, String password) {
         super(name, email, password);
@@ -16,6 +17,14 @@ public class Administrator extends Person {
 
     public void setCode(int code) {
         this.code = code;
+    }
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(ArrayList<User> users) {
+        this.users = users;
     }
 
 }
