5a6bc7a6159a84b9bf2a752e1633bebf336a7a82
Bibian Corredor <bibian.corredor@gmail.com>
2023-12-05 18:07:58 -0500

    change some managed controller methods

--- src/co/edu/uptc/controller/AdministratorController.java
+++ src/co/edu/uptc/controller/AdministratorController.java
@@ -45,44 +45,6 @@ public class AdministratorController {
         }
     }
 
-    public boolean emailValidation(String email) {
-
-        ArrayList<String> listDominio = new ArrayList<>();
-        listDominio.add("@gmail.com");
-        listDominio.add("@uptc.edu.co");
-        listDominio.add("@outlook.es");
-        listDominio.add("@yahoo.com");
-
-        for (String s : listDominio) {
-            if (email.contains(s)) {
-                int position = email.length() - s.length();
-                String aux = email.substring(0, position);
-
-                if (aux.contains("@") || aux.length() < 5) {
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-
-        }
-        return false;
-    }
-
-    public boolean passwordValidation(String password) {
-
-        if (password.length() > 3 && password.length() < 20) { // >3 <20
-            if (!password.equals(password.toLowerCase())) { // min. una mayuscula
-                if (!password.equals(password.toUpperCase())) { // min. una miniscula
-                    if (password.matches(".*\\d.*\\d.*")) { // min. 2 numeros
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
     public boolean addSerie(String title, String description, String category, LocalDate publication) {
         if (!title.isEmpty() && !description.isEmpty() && !category.isEmpty()) {
             multimediaGallety.setSeries(new Serie(title, description, category, publication, false));
