a396907fa8a6efd34b3090cc4f92ce861be5c6e4
juan-Riapira <juan.fernandez07@uptc.edu.co>
2023-12-07 12:28:20 -0500

    se corrige metodo de crear usuario

--- src/co/edu/uptc/controller/UserController.java
+++ src/co/edu/uptc/controller/UserController.java
@@ -1,5 +1,44 @@
 package co.edu.uptc.controller;
 
-public class UserController {
+import java.util.ArrayList;
 
+import co.edu.uptc.model.Administrator;
+import co.edu.uptc.model.Plan;
+import co.edu.uptc.model.User;
+
+public class UserController {
+    private Administrator administrator;
+
+    ArrayList<User> users = new ArrayList<>();
+
+    public boolean addUser(String name, String email, String password, String userName, Plan plan) {
+        User user = new User(name, email, password, userName, plan);
+        if (user.getFirstName().isEmpty() && user.getEmail().isEmpty() && user.getPassword().isEmpty()
+                && user.getPlan() != null) {
+            users.add(user);
+            administrator.setUsers(users);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean authentication(String email, String password) {
+
+        User user = findUser(email);
+        if (user != null) {
+            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public User findUser(String email) {
+        for (User user : users) {
+            if (user.getEmail().equals(email)) {
+                return user;
+            }
+        }
+        return null;
+    }
 }
