5c2510c01c8baaa1ed6a2baf571dbeed2ab2e884
Dani Guevara <laura.guevara02@uptc.edu.co>
2023-12-10 21:52:28 -0500

    PM-52 2.0 creation of Category Class

---
+++ src/co/edu/uptc/model/Category.java
@@ -0,0 +1,38 @@
+package co.edu.uptc.model;
+
+import java.util.ArrayList;
+
+public class Category {
+
+    private String category;
+    private ArrayList<Movie> movies;
+    private ArrayList<Serie> series;
+
+    public Category(String category) {
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public ArrayList<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(ArrayList<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public ArrayList<Serie> getSeries() {
+        return series;
+    }
+
+    public void setSeries(ArrayList<Serie> series) {
+        this.series = series;
+    }
+}
