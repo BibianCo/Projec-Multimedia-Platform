6b67aa4e245a8ab7c4cd7c8242927e2e6b14a802
Bibian Corredor <bibian.corredor@gmail.com>
2024-02-04 15:39:27 -0500

    feat: add one-to-many association with Serie and Season

--- src/co/edu/uptc/model/Serie.java
+++ src/co/edu/uptc/model/Serie.java
@@ -1,12 +1,24 @@
 package co.edu.uptc.model;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Serie extends Multimedia {
 
+    private ArrayList<Season> seasons;
+
     public Serie(int id, String title, String synopsis, Date releaseDate) {
         super(id, title, synopsis, releaseDate);
-        // TODO Auto-generated constructor stub
+        this.seasons = new ArrayList<>();
+
+    }
+
+    public ArrayList<Season> getSeasons() {
+        return seasons;
+    }
+
+    public void setSeasons(ArrayList<Season> seasons) {
+        this.seasons = seasons;
     }
 
 }
