5c4e03a69d42862f81c75d781c02b53f38165443
Bibian Corredor <bibian.corredor@gmail.com>
2024-02-04 15:28:49 -0500

    feat: add one-to-many association with Series and Category

--- src/co/edu/uptc/model/Serie.java
+++ src/co/edu/uptc/model/Serie.java
@@ -1,12 +1,24 @@
 package co.edu.uptc.model;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Serie extends Multimedia {
 
+    private ArrayList<Category> categories;
+
     public Serie(int id, String title, String synopsis, Date releaseDate) {
         super(id, title, synopsis, releaseDate);
+        this.categories = new ArrayList<>();
         // TODO Auto-generated constructor stub
+    }
+
+    public ArrayList<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(ArrayList<Category> categories) {
+        this.categories = categories;
     }
 
 }
