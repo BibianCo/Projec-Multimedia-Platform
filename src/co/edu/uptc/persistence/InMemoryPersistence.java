84590021e51aae8704a8ebdbd261d6a5e52ffa63
Maria Fernanda Angarita <maria.angarita02@uptc.edu.co>
2024-02-06 20:17:41 -0500

    Realizacion de Persistence con InMemoryPersistence

--- src/co/edu/uptc/persistence/InMemoryPersistence.java
+++ src/co/edu/uptc/persistence/InMemoryPersistence.java
@@ -2,7 +2,7 @@ package co.edu.uptc.persistence;
 
 import java.util.ArrayList;
 
-public class InMemoryPersistence<T> {
+public class InMemoryPersistence<T> implements Persistence<T>{
 
     public ArrayList<T> data = new ArrayList<T>();
 
@@ -13,4 +13,23 @@ public class InMemoryPersistence<T> {
         this.data = data;
     }
 
+    @Override
+    public boolean persist(T value) {
+        return false;
+    }
+
+    @Override
+    public boolean erase(int id) {
+        return false;
+    }
+
+    @Override
+    public T obtainById(int id) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<T> obtainAll() {
+        return new ArrayList<>();
+    }
 }
