71c83ac6d67d0dc44555677797c8dfcb1fa756a1
Juan-Fernandez <juan.fernandez07@uptc.edu.co>
2024-02-06 22:20:12 -0500

    get functionality added

---
+++ test/co/edu/uptc/controller/CategoryControllerTest.java
@@ -0,0 +1,40 @@
+package co.edu.uptc.controller;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import co.edu.uptc.model.Category;
+import co.edu.uptc.persistence.InMemoryPersistence;
+
+public class CategoryControllerTest {
+
+    public static CategoryController controller;
+    public static InMemoryPersistence<Category> inMemoryPersistence;
+
+    @BeforeClass
+    public static void setUp() {
+        inMemoryPersistence = new InMemoryPersistence<Category>();
+        controller = new CategoryController(inMemoryPersistence);
+
+    }
+
+    @Test
+    public void testGet() {
+        Category c1 = new Category(1, "Terror");
+        Category c2 = new Category(2, "Comedia");
+        Category c3 = new Category(3, "Drama");
+        inMemoryPersistence.data.add(c1);
+        inMemoryPersistence.data.add(c2);
+        inMemoryPersistence.data.add(c3);
+
+        assertEquals(c1, controller.get(1));
+        assertNull(controller.get(4));
+    }
+
+}
