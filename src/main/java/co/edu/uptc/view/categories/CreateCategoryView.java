c3fe6ace84e818d2b48d9b1e3e898decfca05961
Bibian Corredor <bibian.corredor@gmail.com>
2024-03-06 21:14:12 -0500

    feat: add create category view

---
+++ src/main/java/co/edu/uptc/view/categories/CreateCategoryView.java
@@ -0,0 +1,68 @@
+package co.edu.uptc.view.categories;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import com.google.gson.reflect.TypeToken;
+
+import co.edu.uptc.controller.CategoryController;
+import co.edu.uptc.model.Category;
+import co.edu.uptc.persistence.FilePersistence;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class CreateCategoryView implements Initializable {
+    @FXML
+    private TextField categoryName;
+
+    @FXML
+    private TableView<Category> tableView;
+
+    @FXML
+    private TableColumn<Category, Integer> idColumn;
+
+    @FXML
+    private TableColumn<Category, String> nameColumn;
+
+    public CategoryController controller;
+    public FilePersistence<Category> filePersistence;
+    private Type type;
+
+    @FXML
+    private void createCategory() throws IOException {
+        Category category = new Category(setId(), categoryName.getText());
+        if (controller.add(category)) {
+            loadItems();
+        }
+    }
+
+    private void loadItems() {
+        tableView.getItems().setAll(new ArrayList<>());
+        tableView.getItems().addAll(controller.getAll());
+    }
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        type = new TypeToken<ArrayList<Category>>() {
+        }.getType();
+        filePersistence = new FilePersistence<>(type, "categories");
+        controller = new CategoryController(filePersistence);
+        filePersistence.createFile();
+        loadItems();
+
+    }
+
+    public int setId() {
+        return controller.getAll().size() + 1;
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,8 +5,13 @@ module co.edu.uptc {
 
     opens co.edu.uptc to javafx.fxml;
     opens co.edu.uptc.view.movies to javafx.fxml;
+    opens co.edu.uptc.view.categories to javafx.fxml;
     opens co.edu.uptc.model to com.google.gson;
 
     exports co.edu.uptc;
+    exports co.edu.uptc.controller;
+    exports co.edu.uptc.model;
+    exports co.edu.uptc.persistence;
     exports co.edu.uptc.view.movies;
+    exports co.edu.uptc.view.categories;
 }
